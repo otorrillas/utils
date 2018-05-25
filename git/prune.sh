2c8a6e1d7e3e3b3a1a7c945964c51b363a02abe3
Oriol Torrillas <otorrillas@gmail.com>
2018-05-25 14:41:16 +0100

    Git advanced prune

---
+++ git/prune.sh
@@ -0,0 +1,3 @@
+git fetch
+git remote prune origin
+git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -D
--- git/rebase-all.fish
+++ git/rebase-all.fish
@@ -1,4 +1,4 @@
-set branches feature/sprint6/OS-122-session-timeout feature/sprint5/OS-47-bmr-smr feature/sprint5/OS-27-help-icon feature/sprint5/OS-1118-Exit-Application feature/sprint4/OS-54-legal-terms
+set branches feature/sprint5/OS-27-help-icon feature/sprint4/OS-54-legal-terms
 
 git fetch
 
