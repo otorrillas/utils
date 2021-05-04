ff5101553826e4c938e4534f81e262b95e82e263
Oriol Torrillas <oriol.torrillas@typeform.com>
2021-05-04 15:41:56 +0200

    feat: add zsh-syntax-highlighting

--- dotenv/install.sh
+++ dotenv/install.sh
@@ -10,6 +10,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 brew install zsh-syntax-highlighting
 git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Fira code: https://github.com/tonsky/FiraCode/wiki/Installing
 brew tap homebrew/cask-fonts
