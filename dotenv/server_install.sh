6dc866ec6c59924c3784867fd918a8f7841dfe98
Oriol Torrillas <otorrillas@gmail.com>
2022-08-02 10:48:51 +0000

    feat: remove nvm

--- dotenv/server_install.sh
+++ dotenv/server_install.sh
@@ -6,8 +6,6 @@ pip3 install thefuck --user
 # ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 ## plugins
-brew install zsh-syntax-highlighting
-git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
