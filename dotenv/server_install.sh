c1135cb380d37fea40edac3ce34f178a0aed92a0
Oriol Torrillas <oriol.torrillas@datadoghq.com>
2022-08-02 12:37:42 +0200

    feat: add server_install script

--- dotenv/server_install.sh
+++ dotenv/server_install.sh
@@ -1,30 +1,7 @@
-# brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-## update & upgrade first
-brew update
-brew upgrade
-
-## iterm2
-brew install --cask iterm2
-
-## dev tools
-brew install git
-brew install git-lfs
-brew install --cask visual-studio-code
-brew install thefuck
-
-## browsers
-brew install --cask firefox
-
-## apps
-brew install --cask spotify
-brew install --cask rectangle
-
-## Fira code: https://github.com/tonsky/FiraCode/wiki/Installing
-brew tap homebrew/cask-fonts
-brew install --cask font-fira-code
-
+# thefuck
+sudo apt update
+sudo apt install python3-dev python3-pip python3-setuptools
+pip3 install thefuck --user
 
 # ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -34,8 +11,6 @@ git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# brew cleanup
-brew cleanup
 
 # apply configs to $HOME
 CURRENT_DIR=$(dirname "$0")
